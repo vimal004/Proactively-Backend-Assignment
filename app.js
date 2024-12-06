9b4bd8f2a1839d10323ab01e2bb30d63fb5ee4e7
vimal004 <2004.vimal@gmail.com>
2024-12-06 15:17:22 +0530

    feat: add public route for fetching speakers and define model associations

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const sequelize = require("./config/database");
-const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const userrouter = require("./routes/auth");
 const protectedrouter = require("./routes/protected");
+const publicrouter = require("./routes/public");
 
 const app = express();
 
 app.use(express.json());
 app.use("/user", userrouter);
 app.use("/protected", protectedrouter);
+app.use("/public", publicrouter);
 
+const { User, SpeakerProfile } = require("./models/associations");
+
+// Sync database
 sequelize
-  .sync({ force: false })
+  .sync({ alter: true })
   .then(() => {
     console.log("Database synced!");
   })
-  .catch((error) => {
-    console.error("Database sync failed:", error);
+  .catch((err) => {
+    console.error("Error syncing database:", err);
   });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
